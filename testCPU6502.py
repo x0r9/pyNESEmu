bcca57986ddcd2977aa85c9a49a95c837a00db05
x0r9 <x0r9@users.noreply.github.com>
2017-07-23 21:39:11 +0100

    add more tests for LDA zeropage

--- testCPU6502.py
+++ testCPU6502.py
@@ -20,6 +20,9 @@ def loadBin(cpu, data):
     
     cpu.pCnt.V = 0x8000
      
+def loadRam(cpu, data):
+    for n, char in enumerate(data):
+        cpu.memory[n] = ord(char)
 
 class TestCPUStatusReg(unittest.TestCase):
     def setUp(self):
@@ -61,6 +64,45 @@ class TestCPUStatusReg(unittest.TestCase):
         self.cpu.Tick()
         self.assertFalse( self.cpu.flagDecimal.V)
         
-
+class TestLoadingA(unittest.TestCase):
+    def setUp(self):
+        """
+        Create the CPU ready for testing...
+        """
+        self.cpu = createCPU()
+        
+    def test_loadALiteral(self):
+        """
+        Test load A literally with an immediate memory type
+        """
+        loadBin(self.cpu, "\xA9\x00\x00")
+        self.cpu.Tick()
+        self.assertEqual(self.cpu.regA.V, 0)
+        
+        loadBin(self.cpu, "\xA9\x01\x00")
+        self.cpu.Tick()
+        self.assertEqual(self.cpu.regA.V, 1)
+        
+        loadBin(self.cpu, "\xA9\xFF\x00")
+        self.cpu.Tick()
+        self.assertEqual(self.cpu.regA.V, 0xFF)
+         
+    def test_loadAZeropage(self):
+        """
+        Test load A with zeropage
+        """
+        loadRam(self.cpu, "\xA5\x5A\xAA\x55")
+        loadBin(self.cpu, "\xA5\x00\x00")
+        self.cpu.Tick()
+        self.assertEqual(self.cpu.regA.V, 0xA5)
+        
+        loadBin(self.cpu, "\xA5\x01\x00")
+        self.cpu.Tick()
+        self.assertEqual(self.cpu.regA.V, 0x5A)
+        
+        loadBin(self.cpu, "\xA5\xFF\x00")
+        self.cpu.Tick()
+        self.assertEqual(self.cpu.regA.V, 0x00)   
+                     
 if __name__ == '__main__':
     unittest.main()
